782d5f021353e91aae941c0b901f1a5e54e51677
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-01-06 00:53:54 -0700

    whups v2

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def on_error(event_method, *args, **kwargs):
     tb = traceback.format_exc()
     error_trace = "".join(tb)
     print(error_trace)
-    embed = discord.Embed(description=error_trace.replace("__", "_\_").replace("**", "*\*"))
+    embed = discord.Embed(description=error_trace.replace("__", "_\\_").replace("**", "*\\*"))
     await bot.err_logs_channel.send("An error occurred while processing `{}`.".format(event_method), embed=embed)
 
 
